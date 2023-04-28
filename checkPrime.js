e6426dca09a48672b41e4bf63d37c423664eb375
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-28 17:36:07 +0530

    Code fixed. Multiple changes.

--- checkPrime.js
+++ checkPrime.js
@@ -1,15 +1,17 @@
 let checkPrime = (num) =>{
-    let factor =0
+    let factor = 0
     for(let i=0;i<=num;i++){
         if(num%i==0){
             factor++
         }
     }
-if(factor==1){
-    return true;
-}else{
-    return false;
+    if(factor==2){
+        return true;
+    }else{
+        return false;
+    }
 }
 
-}
+let result = checkPrime(17);
+console.log(result);
 
